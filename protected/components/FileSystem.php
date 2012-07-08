2db2549151d8f6506f599a05c77221d7200b3f57
Andrey <to.merge@gmail.com>
2012-07-08 19:58:46 +0600

    fs bulk

---
+++ protected/components/FileSystem.php
@@ -0,0 +1,36 @@
+<?php
+
+class FileSystem extends CComponent
+{
+	/**
+	 * @param string $uid
+	 * @return string full path in filesystem to file
+	 */
+	public function getFilePath($uid) {
+
+	}
+
+	/**
+	 * @param $uid
+	 * @return string Url to file
+	 */
+	public function getFileUrl($uid) {
+
+	}
+
+	/**
+	 * @param string $fileName
+	 * @return string Uid of new file
+	 */
+	public function publishFile($fileName) {
+
+	}
+
+	/**
+	 * @param string $uid
+	 * @return bool
+	 */
+	public function removeFile($uid) {
+
+	}
+}
